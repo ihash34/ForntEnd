47ecb266d2f9eab7c5cc5f7e2e29587e65f8c34f
ihash34 <i.hash34@gmai.com>
2019-09-23 16:49:34 +0500

    Inner Tabs Functionting

--- JavaScript/dragscroll.js
+++ JavaScript/dragscroll.js
@@ -154,7 +154,7 @@ $(function() {
     $('#right-button').click(function() {
       var leftPos = outer.scrollLeft();
       outer.animate({
-        scrollLeft: leftPos - 200
+        scrollLeft: leftPos - 100
       }, 800, function() {
         debugger;
         if ($('#outer').scrollLeft() <= 0) {
@@ -167,7 +167,7 @@ $(function() {
       setVisible($('#right-button'));
       var leftPos = outer.scrollLeft();
       outer.animate({
-        scrollLeft: leftPos + 200
+        scrollLeft: leftPos + 100
       }, 800);
     });
 
@@ -175,3 +175,42 @@ $(function() {
       updateUI();
     });
   });
+
+
+//------------- T A B S --------------------------------------
+
+function setupTabs(){
+  document.querySelectorAll(".tabs__button").forEach(button=>{
+  button.addEventListener ("click",()=>{
+  
+    const sideBar=button.parentElement;
+    const tabsContainer=sideBar.parentElement;
+     const tabNumber=button.dataset.forTab;
+    const tabToActivate=tabsContainer.querySelector('.tabs__content[data-tab='+'"'+tabNumber+'"'+"]");
+
+          sideBar.querySelectorAll(".tabs__button").forEach(button=>{
+         button.classList.remove("tabs__button--active"); 
+        });
+  
+          tabsContainer.querySelectorAll(".tabs__content").forEach(tab=>{
+         tab.classList.remove("tabs__content--active");
+        });
+  
+
+
+        button.classList.add("tabs__button--active");
+        tabToActivate.classList.add("tabs__content--active");
+        
+  });
+ });
+  }
+  
+  document.addEventListener("DOMContentLoaded",()=>{
+  setupTabs();
+  
+  document.querySelectorAll(".tabs").forEach(tabsContainer=>{
+    tabsContainer.querySelector(".tabs__sideBar .tabs__button").click();
+     });
+  
+});
+//----------------------------------------------------------------------------
